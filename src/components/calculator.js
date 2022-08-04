ef76d9511a11a1fdcc34c680e39f411b40b6c58d
ifzyy <johnsonnifemi8@gmail.com>
2022-08-04 22:51:56 +0100

    added events

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,69 +1,42 @@
-import React from 'react';
+/* eslint-disable react/prefer-stateless-function */
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
-function Operations() {
+const Calculator = () => {
+  const [calc, setCalc] = useState({ total: '', operation: '', next: '' });
+
+  const handleClick = (e) => {
+    setCalc(calculate(calc, e.target.innerText));
+  };
+
   return (
-    <div className="operators">
-      <button type="button" className="symbol" id="divide">&divide;</button>
-      <button type="button" className="symbol" id="multiply">&times;</button>
-      <button type="button" className="symbol" id="minus">-</button>
-      <button type="button" className="symbol" id="minus">+</button>
-      <button type="button" className="symbol" id="equals">=</button>
-    </div>
+    <main>
+      <section className="calculator">
+        <p className="display">{(calc.total || '') + (calc.operation || '') + (calc.next || '') || 0}</p>
+        <button type="button" id="AC" onClick={handleClick}>AC</button>
+        <button type="button" id="+/-" onClick={handleClick}>+/-</button>
+        <button type="button" id="%" onClick={handleClick}>%</button>
+        <button type="button" className="operator" id="&divide;" onClick={handleClick}>&divide;</button>
+        <button type="button" id="7" onClick={handleClick}>7</button>
+        <button type="button" id="8" onClick={handleClick}>8</button>
+        <button type="button" id="9" onClick={handleClick}>9</button>
+        <button type="button" className="operator" id="x" onClick={handleClick}>x</button>
+        <button type="button" id="4" onClick={handleClick}>4</button>
+        <button type="button" id="5" onClick={handleClick}>5</button>
+        <button type="button" id="6" onClick={handleClick}>6</button>
+        <button type="button" className="operator" id="-" onClick={handleClick}>-</button>
+        <button type="button" id="1" onClick={handleClick}>1</button>
+        <button type="button" id="2" onClick={handleClick}> 2 </button>
+        <button type="button" id="3" onClick={handleClick}> 3 </button>
+        <button type="button" className="operator" id="+" onClick={handleClick}>+</button>
+        <button type="button" className="zero" id="0" onClick={handleClick}>0</button>
+        <button type="button" id="." onClick={handleClick}>.</button>
+        <button type="button" className="operator" id="=" onClick={handleClick}>=</button>
+      </section>
+    </main>
   );
-}
+};
 
-function Numbers() {
-  return (
-    <div className="numberKeys">
-      <button type="button">7</button>
-      <button type="button">8</button>
-      <button type="button">9</button>
-      <button type="button">4</button>
-      <button type="button">5</button>
-      <button type="button">6</button>
-      <button type="button">1</button>
-      <button type="button">2</button>
-      <button type="button">3</button>
-      <button type="button" className="zero">0</button>
-      <button type="button" className="dot">.</button>
-    </div>
-  );
-}
-
-function GeneralOperations() {
-  return (
-    <div className="controls">
-      <button type="button">AC</button>
-      <button type="button">+/-</button>
-      <button type="button">%</button>
-    </div>
-  );
-}
-
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.result = 0;
-  }
-
-  render() {
-    return (
-      <main>
-        <section className="calculator">
-          <p className="results">{this.result}</p>
-          <div className="gOpsBody">
-            <GeneralOperations />
-          </div>
-          <div className="numBody">
-            <Numbers />
-          </div>
-          <div className="sideOps">
-            <Operations />
-          </div>
-        </section>
-      </main>
-    );
-  }
-}
+Calculator.defaultProps = { total: 0, operations: '', next: 0 };
 
 export default Calculator;
